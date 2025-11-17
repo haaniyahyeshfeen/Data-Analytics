9134f58e45bafd9750dbdc272f31bb1acdfcc47b
haaniyahyeshfeen <haaniyahyeshfeen2005@gmail.com>
2025-11-17 13:06:08 +0530

    Add files via upload

---
+++ samp.py
@@ -0,0 +1,23 @@
+BOOLEAN
+a = True
+
+INTEGER
+num = 10
+
+FLOAT
+num = 4.55
+
+STRING
+name = "Hanu"
+
+LIST
+fruits = ["apple", "banana", "mango"]
+
+TUPLES
+point = (4, 5)
+
+DICTIONARY
+student = {"name": "HANU", "age": 20}
+
+SET
+numbers = {1, 2, 3}
